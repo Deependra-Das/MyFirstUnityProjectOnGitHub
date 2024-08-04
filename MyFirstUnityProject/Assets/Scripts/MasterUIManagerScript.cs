1f9394962f1b69af1712169f920c21982abd1a7f
Deependra-Das <iamdeep75@gmail.com>
2024-08-05 05:19:15 +0530

    Unity UI Assignment with Git
    - Added Master Scene with 3 buttons to go to respective scenes
    - Added Scene 1 with button to go back to Master Scene
    - Added Scene 2 with button to go back to Master Scene
    - Added Scene 3 with button to go back to Master Scene
    - Added respective scripts for the button interactions & Scene Management

---
+++ MyFirstUnityProject/Assets/Scripts/MasterUIManagerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MasterUIManagerScript : MonoBehaviour
+{
+    public Button button_scene_1;
+    public Button button_scene_2;
+    public Button button_scene_3;
+    public string scene_1_name_text;
+    public string scene_2_name_text;
+    public string scene_3_name_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button_scene_1.onClick.AddListener(onButton1Clicked);
+        button_scene_2.onClick.AddListener(onButton2Clicked);
+        button_scene_3.onClick.AddListener(onButton3Clicked);
+    }
+
+    private void onButton1Clicked()
+    {
+        SceneManager.LoadScene(scene_1_name_text);
+    }
+
+    private void onButton2Clicked()
+    {
+        SceneManager.LoadScene(scene_2_name_text);
+
+    }
+
+    private void onButton3Clicked()
+    {
+        SceneManager.LoadScene(scene_3_name_text);
+    }
+
+}
---
+++ MyFirstUnityProject/Assets/Scripts/SceneUIManagerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneUIManagerScript : MonoBehaviour
+{
+    public Button button_scene;
+    public string scene_master_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button_scene.onClick.AddListener(onButtonBackToMasterClicked);
+    }
+
+    private void onButtonBackToMasterClicked()
+    {
+        SceneManager.LoadScene(scene_master_text);
+    }
+
+}
